18070276133e66e1bb5b8cbcd4d84089f97bde3f
Justyn Roberts <justynroberts@gmail.com>
2021-08-27 10:59:25 +0100

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 SERVICE="SSH Login"
 USER=$LOGNAME
-TEXT="Windows login for $USER on $SSH_CONNECTION"
+TEXT="SSH Login  $USER on $SSH_CONNECTION"
 DOCUMENT='{"title": "🚀  '$SERVICE' ['$USER']", "text":"'$TEXT'", "duration": 100, "severity": -1}'
 #echo "$DOCUMENT" - Debug time :)
 curl -XPOST http://127.0.0.1:42699/com.instana.plugin.generic.event -H "Content-Type: application/json" -d "$DOCUMENT"
